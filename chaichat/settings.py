6ad09474999a9e980d27690b4fde3a3cd5887212
Oviyan007 <Oviyan.btech@gmail.com>
2025-08-09 13:18:28 +0530

    remove credential

--- chaichat/settings.py
+++ chaichat/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'oviyanlearnsai@gmail.com'       # Your Gmail
-EMAIL_HOST_PASSWORD = 'huhv ssdg pynx khue'  
+EMAIL_HOST_USER = ''       # Your Gmail
+EMAIL_HOST_PASSWORD = ''  
